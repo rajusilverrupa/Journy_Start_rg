0adef0bea7740631c7f3be89b5296939245c4906
rajurangeela <rajusharmaei44@gmail.com>
2024-11-20 17:47:43 +0530

    change

--- RG/provider.tf
+++ RG/provider.tf
@@ -1,13 +1,12 @@
 terraform {
-    required_providers {
-      azurerm={
-        source = "hashicorp/azurerm"
-        version = "~>=4.0.1"
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.1"
     }
-}
-provider "azurerm" {
-  features {
-    
   }
+}
+
+provider "azurerm" {
+  features {}
 }
